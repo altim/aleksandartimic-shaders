f38dd14829ec38d2a5e405f77a2ec801dca049c5
Aleksandar Timic <aleksandartimic@gmail.com>
2026-02-14 20:09:50 +0100

    Responsivness fixes

--- app/layout.tsx
+++ app/layout.tsx
@@ -56,6 +56,7 @@ export const metadata: Metadata = {
   icons: {
     icon: "/icon.svg",
   },
+  viewport: "width=device-width, initial-scale=1, viewport-fit=cover",
 };
 
 export default function RootLayout({
